61c59fb3b1d1b7c53e89127658de833c706903c4
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-02-10 14:17:18 +0100

    2 arrays that checks years with stat and without

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -20,40 +20,35 @@ export default class Chart extends Component {
     return (
       <div style={{ display: "flex", flexDirection: "column" }}>
         <div style={{ display: "flex" }}>
-          <div style={{ width: "100px", textAlign: "center" }} />
+          <div style={{ width: "80px", textAlign: "center" }} />
           {[2016, 2017, 2018, 2019, 2020, 2021, 2022].map((year) => (
-            <div style={{ width: "100px", textAlign: "center" }}>{year}</div>
+            <div style={{ width: "80px", textAlign: "center" }}>{year}</div>
           ))}
         </div>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <button onClick={() => this.handleClick("retention")}>
-            Retention
-          </button>
-          <button onClick={() => this.handleClick("interest")}>Interest</button>
-          <button onClick={() => this.handleClick("usage")}>Usage</button>
-          <button onClick={() => this.handleClick("awareness")}>
-            Awareness
-          </button>
-        </div>
+      
 
-        {frameworks.map((framework) => (
-          <div style={{ display: "flex" }}>
-            <div style={{ width: "100px", textAlign: "center" }}>
-              {framework.name}
-            </div>
-            {/* skappa tomma divar respektive name så position blir rätt i kollum  t.tc preac 2018 - 2016 */}
-            {framework.surveys.map((survey) => (
-              <div
-                class="numbers"
-                style={{ width: "100px", textAlign: "center" }}
-              >
-                {survey[this.state.attribute]
-                  ? survey[this.state.attribute]
-                  : "-"}
-              </div>
-            ))}
-          </div>
-        ))}
+        {frameworks.map((framework) => {
+  const yearsWithStatistics = framework.surveys.map(survey => survey.year)
+  const missingYears = [2016, 2017, 2018, 2019, 2020, 2021, 2022].filter(
+    year => !yearsWithStatistics.includes(year)
+  )
+  return (
+    <div class="test" style={{ display: "flex" }}>
+      <div style={{ width: "80px", textAlign: "center" }}>
+        {framework.name}
+      </div>
+      {missingYears.map(year => (
+        <div class="numbers" style={{ width: "80px", textAlign: "center" }} />
+      ))}
+      {framework.surveys.map((survey) => (
+        <div class="numbers" style={{ width: "80px", textAlign: "center" }}>
+          {survey[this.state.attribute] ? survey[this.state.attribute] : "-"}
+        </div>
+      ))}
+    </div>
+  )
+})}
+
       </div>
     )
   }
