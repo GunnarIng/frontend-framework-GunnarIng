ec942beefb5f79ef8baefaff1ba21464f2cca386
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-02-14 21:16:38 +0100

    made progress in cypress. Still problem with the line

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -18,15 +18,15 @@ export default class Chart extends Component {
 
   render() {
     return (
-      <div 
-      class="data-chart"
-      style={{ display: "flex", flexDirection: "column" }}
+      <div
+        class="data-chart"
+        style={{ display: "flex", flexDirection: "column" }}
       >
         <div data-cy="chart-header" class="years-row">
           <div />
           {[2016, 2017, 2018, 2019, 2020, 2021, 2022].map((year) => (
             <div style={{ width: "50px", textAlign: "center" }}>{year}</div>
-            ))}
+          ))}
           <div />
         </div>
 
@@ -34,18 +34,24 @@ export default class Chart extends Component {
           const yearsWithStatistics = framework.surveys.map(
             (survey) => survey.year
             )
-            const missingYears = [
-              2016, 2017, 2018, 2019, 2020, 2021, 2022,
-            ].filter((year) => !yearsWithStatistics.includes(year))
-            return (
-              <div data-cy="chart-row" class="frame-names">
-              <div data-cy="row-line" class="line" style={{ color: framework.color }}></div>
-              <div  class="framework" style={{ color: framework.color }}>
-                <div data-cy="chart-cell" class="frameworks-name">{framework.name}</div>
+          const missingYears = [
+            2016, 2017, 2018, 2019, 2020, 2021, 2022,
+          ].filter((year) => !yearsWithStatistics.includes(year))
+          return (
+            <div data-cy="chart-row" class="frame-names">
+              <div
+                data-cy="row-line"
+                class="line"
+                style={{ backgroundColor: framework.color }}
+              ></div>
+              <div class="framework" style={{ color: framework.color }}>
+                <div data-cy="chart-cell" class="frameworks-name">
+                  {framework.name}
+                </div>
               </div>
 
               {missingYears.map((year) => (
-                <div 
+                <div
                 style={{
                   color: framework.color,
                   width: "54px",
@@ -55,9 +61,8 @@ export default class Chart extends Component {
                 ))}
 
               {framework.surveys.map((survey) => (
-                <div data-cy="chart-cell" >
+                <div data-cy="chart-cell">
                   <div
-                  
                     data-cy="chart-circle"
                     class="circles"
                     style={{ color: framework.color }}
@@ -72,11 +77,15 @@ export default class Chart extends Component {
                   </div>
                 </div>
               ))}
-          
-              <div data-cy="chart-cell"  class="frame-names-end" style={{ color: framework.color }}>
+
+              <div
+                data-cy="chart-cell"
+                class="frame-names-end"
+                style={{ color: framework.color }}
+              >
                 {framework.name}
               </div>
-            
+
               {/* <Footer onAttributeChange={this.handleAttributeChange} /> */}
             </div>
           )
