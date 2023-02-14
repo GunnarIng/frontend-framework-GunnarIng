1fd60c55404fb521462f2f3e3676b72998ac025e
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-02-14 14:51:34 +0100

    style for footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 
 export default function Footer(props) {
   return (
-    <div>
+    <div class="container">
       <button onClick={() => props.onAttributeChange("retention")}>Retention</button>
       <button onClick={() => props.onAttributeChange("interest")}>Interest</button>
       <button onClick={() => props.onAttributeChange("usage")}>Usage</button>
