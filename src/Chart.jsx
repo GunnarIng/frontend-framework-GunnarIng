e4dcd61dcda478735dccc176a2a398ee5f6f95c3
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-02-15 17:08:53 +0100

    started with route and fixing the lines to be scrollable

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,9 +1,14 @@
+import {useRouter} from "preact-router"
 import { useEffect, useState } from "preact/hooks"
 import { frameworks } from "../data/index.js"
 import "./Chart.css"
 
 export default function Chart() {
-  const [attribute, setAttribute] = useState("retention")
+  const [attribute, setAttribute] = useState("usage")
+
+  const router = useRouter()
+  
+  console.log(router)
 
   // This useEffect hook listens for changes in the URL query parameters and updates the attribute state variable accordingly
   useEffect(() => {
@@ -28,15 +33,17 @@ export default function Chart() {
         const yearsWithStatistics = framework.surveys.map((survey) => survey.year)
         const missingYears = [2016, 2017, 2018, 2019, 2020, 2021, 2022].filter(
           (year) => !yearsWithStatistics.includes(year)
-        )
-        return (
-          <div data-cy="chart-row" class="frame-names">
-            <div data-cy="row-line" class="line" style={{ backgroundColor: framework.color }}></div>
+          )
+          return (
+            <div data-cy="chart-row" class="frame-names">
+            
+            <div data-cy="row-line" class="line" style={{ backgroundColor: framework.color, color: framework.color }}></div>
+              
             <div class="framework" style={{ color: framework.color }}>
               <div
                 data-cy="chart-cell"
                 class="left-frameworks-names {
-"
+                  "
               >
                 {framework.name}
               </div>
@@ -44,14 +51,14 @@ export default function Chart() {
 
             {missingYears.map((year) => (
               <div
-                data-cy="chart-cell"
-                style={{
-                  color: framework.color,
-                  width: "54px",
-                  textAlign: "center",
+              data-cy="chart-cell"
+              style={{
+                color: framework.color,
+                width: "54px",
+                textAlign: "center",
                 }}
-              />
-            ))}
+                />
+                ))}
 
             {framework.surveys.map((survey) => (
               <div data-cy="chart-cell">
