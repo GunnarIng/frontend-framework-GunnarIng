6fa6662f4f3b3ef843a1b773f6894f383c73446c
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-02-16 16:32:20 +0100

    Comment code and cleaned css

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -3,17 +3,15 @@ import { frameworks } from "../data/index.js"
 import "./Chart.css"
 
 export default function Chart() {
-
-
-
-  const [router, setRouter] = useRouter()
+  // 
+  const [router] = useRouter()
 
   console.log(router.matches.attribute)
 
-
-
   return (
+    // This is the chart
     <div class="data-chart" style={{ display: "flex", flexDirection: "column" }}>
+      {/* This is the header in the chart with the years in it */}
       <div data-cy="chart-header" class="years-row">
         <div />
         {[2016, 2017, 2018, 2019, 2020, 2021, 2022].map((year) => (
@@ -21,52 +19,55 @@ export default function Chart() {
         ))}
         <div />
       </div>
-
+         
       {frameworks.map((framework) => {
+        // This creates an array of years that have statistics
         const yearsWithStatistics = framework.surveys.map((survey) => survey.year)
+        // This creates an array of years that are missing statistics
         const missingYears = [2016, 2017, 2018, 2019, 2020, 2021, 2022].filter(
           (year) => !yearsWithStatistics.includes(year)
         )
         return (
-          <div data-cy="chart-row" class="frame-names">
+          <div data-cy="chart-row" class="chart-rows">
             <div
               data-cy="row-line"
               class="line"
               style={{ backgroundColor: framework.color, color: framework.color }}
             ></div>
 
-            <div class="framework" style={{ color: framework.color }}>
-              <div
-                data-cy="chart-cell"
-                class="left-frameworks-names {
-                  "
-              >
+            <div style={{ color: framework.color }}>
+              {/* Left Framework names */}
+              <div data-cy="chart-cell" class="left-frameworks-names">
                 {framework.name}
               </div>
             </div>
 
+            {/* Creates empty divs from years that dont have statistics */}
             {missingYears.map((year) => (
               <div
+                class="missing-years"
                 data-cy="chart-cell"
                 style={{
                   color: framework.color,
-                  width: "54px",
-                  textAlign: "center",
                 }}
               />
             ))}
-
+            {/* Creates divs with values from years that have statistics */}
             {framework.surveys.map((survey) => (
               <div data-cy="chart-cell">
                 <div data-cy="chart-circle" class="circles" style={{ color: framework.color }}>
                   <span class="numbers">
-                    {/* This reads out each value and add a % symbol */}
-                    {survey[router.matches.attribute] != null ? `${survey[router.matches.attribute]}%` : null}
+                    {/*Here the value are stored depending on wich attribute that are read. 
+                    The router reads from this also so the links show right att.  */}
+                    {survey[router.matches.attribute] != null
+                      ? `${survey[router.matches.attribute]}%`
+                      : null}
                   </span>
                 </div>
               </div>
             ))}
 
+            {/* Right Framwork names and gets the names from {framewor.name} */}
             <div
               data-cy="chart-cell"
               class="right-frameworks-names"
